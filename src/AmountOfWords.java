49174807ebf8d876551d499eee0459c761669301
Ezekiel <umanmah234@gmail.com>
2023-03-02 19:13:31 -0500

    get the number of words

---
+++ src/AmountOfWords.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class AmountOfWords {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Please, enter any text: ");
+
+        int amountOfWords = getWordsAmount(sc.nextLine());
+
+        System.out.println("Amount of words in your text: " + amountOfWords);
+    }
+
+    public static int getWordsAmount(String text) {
+        String[] textArr = text.split("\\s+");
+        return textArr.length;
+    }
+}
