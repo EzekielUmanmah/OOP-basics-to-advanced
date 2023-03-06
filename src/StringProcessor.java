00e941e802ee54cbb24be6868da7ac3b43010e4b
Ezekiel <umanmah234@gmail.com>
2023-03-05 20:37:59 -0500

    solve string processor

---
+++ src/StringProcessor.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class StringProcessor {
+    public static final String INPUT_DATA = "Login;Name;Email" + System.lineSeparator() +
+            "peterson;Chris Peterson;peterson@outlook.com" + System.lineSeparator() +
+            "james;Derek James;james@gmail.com" + System.lineSeparator() +
+            "jackson;Walter Jackson;jackson@gmail.com" + System.lineSeparator() +
+            "gregory;Mike Gregory;gregory@yahoo.com";
+
+    public static void main(String[] args) {
+//        System.out.println(INPUT_DATA);
+        System.out.println("===== Convert 1 demo =====");
+        System.out.println(convert1(INPUT_DATA));
+
+        System.out.println("===== Convert 2 demo =====");
+        System.out.println(convert2(INPUT_DATA));
+
+    }
+
+    public static String convert1(String input) {
+        StringBuilder output = new StringBuilder();
+        String[] inputs = input.split(System.lineSeparator());
+
+        for(int i = 1; i < inputs.length; i++){
+            String[] temp = inputs[i].split(";");
+            output.append(temp[0]).append(" ==> ").append(temp[2]).append(System.lineSeparator());
+        }
+        return output.toString();
+    }
+
+
+    public static String convert2(String input) {
+        StringBuilder str = new StringBuilder();
+        String[] inputs = input.split(System.lineSeparator());
+
+        for(int i = 1; i < inputs.length; i++){
+            String[] temp = inputs[i].split(";");
+
+            str.append(temp[1]).append(" (email: ").append(temp[2]).append(")").append(System.lineSeparator());
+        }
+        return str.toString();
+    }
+
+}
